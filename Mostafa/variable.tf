da9c32443dea4988dd9be5148777a0d8abec67f5
MM-Tamer <frost2152@gmail.com>
2024-08-08 21:58:30 +0300

    added nat gateway and elastic ip resources

--- Mostafa/variable.tf
+++ Mostafa/variable.tf
@@ -14,3 +14,32 @@ variable "subnet_cidr_block" {
     default = [ "10.0.0.0/20", "10.0.16.0/20", "10.0.128.0/20", "10.0.144.0/20", "10.0.160.0/20" ]
 }
 
+variable "public_subnet_cidr_block_1" {
+    type = string
+    default = "10.0.0.0/20"
+}
+
+variable "public_subnet_cidr_block_2" {
+    type = string
+    default = "10.0.16.0/20"
+}
+
+variable "private_subnet_cidr_block_1" {
+    type = string
+    default = "10.0.128.0/20"
+}
+
+variable "private_subnet_cidr_block_2" {
+    type = string
+    default = "10.0.144.0/20"
+}
+
+variable "private_subnet_cidr_block_3" {
+    type = string
+    default = "10.0.160.0/20"
+}
+
+variable "private_subnet_cidr_block_4" {
+    type = string
+    default = "10.0.176.0/20"
+}
