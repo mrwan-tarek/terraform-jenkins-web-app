5b028b7632fcea292eb8196ca91db34e955c7746
SaRa-Fathi <sarafathi607@gmail.com>
2024-08-16 23:27:37 +0300

    app form inputs

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>User Form</title>
+</head>
+<body>
+    <form action="submit.php" method="post">
+        <label for="name">Name:</label>
+        <input type="text" name="name" required><br>
+        <label for="age">Age:</label>
+        <input type="number" name="age" required><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
