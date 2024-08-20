01494ca4d874f62ed3e06e9f2ac08f08a0cdb125
SaRa-Fathi <sarafathi607@gmail.com>
2024-08-20 16:46:28 +0300

    create file of submitting form

---
+++ submit.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+
+$stmt = $conn->prepare("INSERT INTO users (name) VALUES (?)");
+$stmt->bind_param("si", $name);
+
+if ($stmt->execute()) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+$stmt->close();
+$conn->close();
+?>
