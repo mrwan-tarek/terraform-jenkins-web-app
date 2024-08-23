65f19b57a370b549f0c10803a18c7aa4d9417335
SaRa-Fathi <sarafathi607@gmail.com>
2024-08-23 19:16:08 +0300

    add search functionality

---
+++ search.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$search = $_POST['search'];
+$stmt = $conn->prepare("SELECT * FROM users WHERE name = ?");
+$stmt->bind_param("s", $search);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    echo "<h2>Search Results:</h2>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<p>Name: " . htmlspecialchars($row['name']) . "<br>Age: " . htmlspecialchars($row['age']) . "<br>Gender: " . htmlspecialchars($row['gender']) . "</p>";
+    }
+} else {
+    echo "No results found.";
+}
+
+$stmt->close();
+$conn->close();
+?>
