ec67c6f5eb32eb050edf3db2503fc993a40ea72b
mrwan-tarek <mrwantarek2010@gmail.com>
2024-08-24 12:05:23 +0300

    trying app branch

--- app-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo yum update -y
-sudo yum install mysql -y
-sudo amazon-linux-extras install docker -y
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-mkdir /home/ec2-user/file
-touch /home/ec2-user/mmmmmm
