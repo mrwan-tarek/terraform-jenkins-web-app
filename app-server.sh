227aa67854a106bb92ff4620aed96c33d3cff034
mrwan-tarek <mrwantarek2010@gmail.com>
2024-08-26 23:42:49 +0300

    editing files

--- app-server.sh
+++ app-server.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install mysql -y
-sudo amazon-linux-extras install docker -y
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-mkdir /home/ec2-user/file
-touch /home/ec2-user/mmmmmm
+sudo yum install git -y
+sudo yum install php-mysqlnd -y
+sudo yum install httpd -y
+git clone --branch app --single-branch https://github.com/mrwan-tarek/terraform-jenkins-web-app.git
+cp /home/ec2-user/terraform-jenkins-web-app/search.php /home/ec2-user/terraform-jenkins-web-app/submit.php /var/www/html/
+chmod -R 755 /var/www/html
+systemctl start httpd
