14c4ce9eda9ba16a219a45f3d3d07e8594bee6d8
mrwan-tarek <mrwantarek2010@gmail.com>
2024-08-26 19:24:04 +0300

    editing app fike

--- search.php
+++ search.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "database-2.cefu4bi34c5q.us-west-2.rds.amazonaws.com";
+$servername = "rds-web-app-db.cefu4bi34c5q.us-west-2.rds.amazonaws.com:3306";
 $username = "admin";
 $password = "1234asAS";
 $dbname = "web_app_db";
