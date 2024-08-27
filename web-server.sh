3397341ddac6aba445b199f2e602dbf7d79845ab
mrwan-tarek <mrwantarek2010@gmail.com>
2024-08-27 18:09:50 +0300

    test file

--- web-server.sh
+++ web-server.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+touch f1
 yum update -y
+touch f2
 yum install git -y
+touch f3
 yum install httpd -y
 git clone --branch app --single-branch https://github.com/mrwan-tarek/terraform-jenkins-web-app.git
 cp  /home/ec2-user/terraform-jenkins-web-app/index.php /var/www/html/
