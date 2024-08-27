ebcdb6d6e81c5757c4e3791088e3d03001d623ab
mrwan-tarek <mrwantarek2010@gmail.com>
2024-08-27 17:33:50 +0300

    editnig file

--- app-server.sh
+++ app-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install git -y
-sudo yum install php-mysqlnd -y
-sudo yum install httpd -y
+yum update -y
+yum install git -y
+yum install php-mysqlnd -y
+yum install httpd -y
 git clone --branch app --single-branch https://github.com/mrwan-tarek/terraform-jenkins-web-app.git
 cp /home/ec2-user/terraform-jenkins-web-app/search.php /home/ec2-user/terraform-jenkins-web-app/submit.php /var/www/html/
 chmod -R 755 /var/www/html
--- web-server.sh
+++ web-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install git -y
-sudo yum install httpd -y
+yum update -y
+yum install git -y
+yum install httpd -y
 git clone --branch app --single-branch https://github.com/mrwan-tarek/terraform-jenkins-web-app.git
 cp  /home/ec2-user/terraform-jenkins-web-app/index.php /var/www/html/
 chmod -R 755 /var/www/html
