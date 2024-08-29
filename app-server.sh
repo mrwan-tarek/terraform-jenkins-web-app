ffc6716119b8771381c1eeee1fc6647146527a90
mrwan-tarek <mrwantarek2010@gmail.com>
2024-08-29 21:52:59 +0300

    edit files

--- app-server.sh
+++ app-server.sh
@@ -4,6 +4,6 @@ yum install git -y
 yum install php-mysqlnd -y
 yum install httpd -y
 git clone --branch app --single-branch https://github.com/mrwan-tarek/terraform-jenkins-web-app.git
-cp /home/ec2-user/terraform-jenkins-web-app/search.php /home/ec2-user/terraform-jenkins-web-app/submit.php /var/www/html/
+cp /terraform-jenkins-web-app/search.php /ec2-user/terraform-jenkins-web-app/submit.php /var/www/html/
 chmod -R 755 /var/www/html
 systemctl start httpd
--- web-server.sh
+++ web-server.sh
@@ -3,6 +3,6 @@ yum update -y
 yum install git -y
 yum install httpd -y
 git clone --branch app --single-branch https://github.com/mrwan-tarek/terraform-jenkins-web-app.git
-cp  /home/ec2-user/terraform-jenkins-web-app/index.php /var/www/html/
-chmod -R 755 /var/www/html
+cp  /terraform-jenkins-web-app/index.php /usr/share/httpd/noindex/index.html
+chmod -R 755 /usr/share/httpd/noindex/
 systemctl start httpd
